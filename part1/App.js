5696aaa473f1f6d0245a2cd8e36e42278d340bb5
Jesse Brink <jbrink0925@gmail.com>
2021-10-09 15:45:32 -0400

    complete part 1

---
+++ part1/App.js
@@ -0,0 +1,10 @@
+const App = () => {
+    return (
+        <div>
+            <FirstComponent />
+            <NamedComponent name="Jesse" />
+        </div>
+    );
+};
+
+ReactDOM.render(<App/>, document.getElementById("root"));
